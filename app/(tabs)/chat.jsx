192240f3f9a888445f222b58c4a17d4c761d9384
DhananjayHirey <147147105+DhananjayHirey@users.noreply.github.com>
2025-04-15 23:04:35 +0530

    Integrated-Tilt-Steering-Feature

--- app/(tabs)/chat.jsx
+++ app/(tabs)/chat.jsx
@@ -2,22 +2,26 @@ import React from 'react'
 import { useState, useEffect } from 'react'
 import { View, StyleSheet,Text,TouchableOpacity } from 'react-native'
 import { ThemedText } from '@/components/ThemedText'
-import {Gyroscope} from 'expo-sensors'
+import {DeviceMotion} from 'expo-sensors'
+import {socket} from '@/utils'
 export default function App() {
-  const [{ x, y, z }, setData] = useState({
-    x: 0,
-    y: 0,
-    z: 0,
+  const [{ alpha, beta, gamma }, setData] = useState({
+    alpha: 0,
+    beta: 0,
+    gamma: 0,
   });
   const [subscription, setSubscription] = useState(null);
+  const [orien,setOrien] = useState('center');
+  const [pressed,setPressed] = useState('');
 
-  const _slow = () => Gyroscope.setUpdateInterval(1000);
-  const _fast = () => Gyroscope.setUpdateInterval(16);
+  const _slow = () => DeviceMotion.setUpdateInterval(1000);
+  const _fast = () => DeviceMotion.setUpdateInterval(16);
 
   const _subscribe = () => {
+    DeviceMotion.setUpdateInterval(16)
     setSubscription(
-      Gyroscope.addListener(gyroscopeData => {
-        setData(gyroscopeData);
+      DeviceMotion.addListener(orienData => {
+        setData(orienData.rotation);
       })
     );
   };
@@ -27,45 +31,104 @@ export default function App() {
     setSubscription(null);
   };
 
+  async function clickUp(){
+    socket.emit('click','w')
+  }
+  async function clickUpOut(){
+    socket.emit('click','w-')
+  }
+  async function clickDown(){
+    socket.emit('click','s')
+  }
+  async function clickDownOut(){
+    socket.emit('click','s-')
+  }
+
+  useEffect(()=>{
+    if(orien=='center'){
+      if(pressed=='left'){
+        socket.emit('click','a-');
+      }
+      else{
+        socket.emit('click','d-');
+      }
+    }
+    else if(orien=='left'){
+      socket.emit('click','a');
+    }
+    else{
+      socket.emit('click','d');
+    }
+  },[orien])
+
+  useEffect(()=>{
+    if(beta<0.2 && beta>-0.2){
+      setOrien('center');
+    }
+    else if(beta>0.2){
+      setOrien('left');
+      setPressed('left');
+    }
+    else{
+      setOrien('right');
+      setPressed('right');
+    }
+  },[beta])
+
   useEffect(() => {
     _subscribe();
     return () => _unsubscribe();
   }, []);
 
   return (
-    <View style={styles.container}>
-      <Text style={styles.text}>Gyroscope:</Text>
-      <Text style={styles.text}>x: {x}</Text>
-      <Text style={styles.text}>y: {y}</Text>
-      <Text style={styles.text}>z: {z}</Text>
-      <View style={styles.buttonContainer}>
-        <TouchableOpacity onPress={subscription ? _unsubscribe : _subscribe} style={styles.button}>
+    <>
+      {/* <Text style={styles.text}>Gyroscope:</Text> */}
+      {/* <Text style={styles.text}>x: {alpha}</Text> */}
+      {/* <Text style={styles.text}>y: {beta}</Text> */}
+      {/* <Text style={styles.text}>z: {gamma}</Text> */}
+      {/* <View style={styles.container} >
+        <Text style={styles.text} >{orien}</Text>
+
+      </View> */}
+      {/* <View style={styles.buttonContainer}> */}
+        {/* <TouchableOpacity onPress={subscription ? _unsubscribe : _subscribe} style={styles.button}>
           <Text>{subscription ? 'On' : 'Off'}</Text>
-        </TouchableOpacity>
-        <TouchableOpacity onPress={_slow} style={[styles.button, styles.middleButton]}>
+        </TouchableOpacity> */}
+        {/* <TouchableOpacity onPress={_slow} style={[styles.button, styles.middleButton]}>
           <Text>Slow</Text>
-        </TouchableOpacity>
-        <TouchableOpacity onPress={_fast} style={styles.button}>
+        </TouchableOpacity> */}
+        {/* <TouchableOpacity onPress={_fast} style={styles.button}>
           <Text>Fast</Text>
-        </TouchableOpacity>
+        </TouchableOpacity> */}
+      {/* </View> */}
+      <View style={{display:'flex', flexDirection:'column', width:'100%', justifyContent:'center', height:'100%'}} >
+          <TouchableOpacity onPressIn={clickUp} onPressOut={clickUpOut} style={{backgroundColor:'white', flexGrow:1}} ></TouchableOpacity>
+          <TouchableOpacity onPressIn={clickDown} onPressOut={clickDownOut} style={{backgroundColor:'gray', flexGrow:1}} ></TouchableOpacity>
       </View>
-    </View>
+    </>
   );
 }
 
 const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    justifyContent: 'center',
-    paddingHorizontal: 10,
+  container:{
+        position:'absolute',
+    transform:[{rotate:'-90deg'}],
+        zIndex:2,
+    left:0,
+    top:'50%',
+    // width:20
   },
   text: {
     textAlign: 'center',
-    color:'white'
+    color:'white',
+    padding:10,
+
+
+    backgroundColor:'black'
   },
   buttonContainer: {
     flexDirection: 'row',
-    alignItems: 'stretch',
+    // alignItems: 'stretch',
     marginTop: 15,
   },
   button: {
--- utils/index.js
+++ utils/index.js
@@ -1,4 +1,4 @@
 import {Platform} from "react-native"
 import {io} from 'socket.io-client'
-export const BaseUrl = Platform.OS==='android'?'exp://172.22.36.155:8081':'http://localhost:3000';
-export const socket = io.connect('http://172.22.36.155:4000')
+export const BaseUrl = Platform.OS==='android'?'exp://172.22.26.74:8081':'http://localhost:3000';
+export const socket = io.connect('http://172.22.26.74:4000')
